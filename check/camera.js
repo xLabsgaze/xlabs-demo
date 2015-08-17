d0a4596680e9ade32f1a0298943b447448fe7552
davidra <divad.nosnilwar@gmail.com>
2015-08-17 12:04:42 +1000

    improved layout of camera check page and tweaked styling. Centered the camera image preventing users having their faces turned to one side.

--- check/camera.js
+++ check/camera.js
@@ -4,8 +4,8 @@ var Camera = {
 
   show : function() {
     document.getElementById( "debugContainer" ).style.display="block";
+    document.getElementById( "debugContainer2" ).style.display="block";
     document.getElementById( "video" ).style.display="inline-block";
-    document.getElementById( "ideas" ).style.display="inline-block";
     document.getElementById( "debug" ).style.display="block";
     xLabs.setConfig( "frame.stream.preview", "1" );
     Camera.allowCamera.show( document.getElementById( "xLabsPreview" ) )
@@ -14,11 +14,11 @@ var Camera = {
   hide : function() {
     xLabs.setConfig( "frame.stream.preview", "0" );
     document.getElementById( "debugContainer" ).style.display="none";
-    document.getElementById( "ideas" ).style.display="none";
+    document.getElementById( "debugContainer2" ).style.display="none";
     document.getElementById( "video" ).style.display="none";
     document.getElementById( "debug" ).style.display="none";
-    console.log( "calling hide" )
     allowCamera.hide() // It'll call hide when the camera is allowed anyway.
-  },
+  }
+
 
 };
